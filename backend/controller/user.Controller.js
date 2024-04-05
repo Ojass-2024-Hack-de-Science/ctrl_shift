07b84ddde22bf678cafdab2f412965c28d0c9510
SudhanshuSingh6 <sudhanshus026@gmail.com>
2024-04-05 22:31:23 +0530

    add booking

--- backend/controller/user.Controller.js
+++ backend/controller/user.Controller.js
@@ -62,3 +62,27 @@ export const userLogin = async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 };
+
+export const createBooking = async(req,res)=>{
+  try {
+    const { userId, driverId, pickupLocation, destination, fare, vehicleType } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    const newBooking = new Booking({
+      user: userId,
+      driver: driverId,
+      pickupLocation: pickupLocation,
+      destination: destination,
+      fare,
+      vehicleType: vehicleType,
+      status: 'pending'
+    });
+    await newBooking.save();
+    res.status(201).json(newBooking);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
